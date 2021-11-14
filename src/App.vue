fdc8151e56f1a044358d2d09933005d898aba72f
kibo13 <kimboris1310@gmail.com>
2021-11-14 23:32:00 +0600

    init: create start project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h2 class="intro">Halloween Website</h2>
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+  },
+}
+</script>
+
+<style lang="sass">
+.intro
+    text-transform: uppercase
+</style>
