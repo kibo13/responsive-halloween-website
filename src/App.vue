e2f5bf8901dd095ee5ffa0cb1ff5f4b2eca31f7b
kibo13 <kimboris1310@gmail.com>
2021-11-17 00:31:52 +0600

    feat: add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
-  <div id="app">
-    <h2 class="intro">Halloween Website</h2>
-    <box-icon type="logo" name="docker"></box-icon>
-    <Home />
-  </div>
+    <div id="app">
+        <div class="header" id="header">
+            <Navbar />
+        </div>
+    </div>
 </template>
 
 <script>
-import Home from './components/Home.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
-  name: 'App',
-  components: {
-    Home,
-  },
+    name: 'App',
+    components: {
+        Navbar,
+    },
 }
 </script>
-
-<style lang="sass">
-.intro
-    text-transform: uppercase
-</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <nav class="nav container">
+            <a href="#" class="nav__logo">
+                <img
+                    src="../assets/images/logo.png"
+                    alt=""
+                    class="nav__logo-img"
+                />
+                Halloween
+            </a>
+
+            <div class="nav__menu" ref="menu">
+                <ul class="nav__list">
+                    <li class="nav__item" v-for="item in items" :key="item.id">
+                        <a
+                            class="nav__link"
+                            :href="`#${item.name}`"
+                            @click="closeMenu"
+                        >
+                            {{ item.name }}
+                        </a>
+                    </li>
+
+                    <a href="#" class="button button-ghost">Support</a>
+                </ul>
+
+                <button class="nav__close" @click="closeMenu">
+                    <box-icon name="x"></box-icon>
+                </button>
+
+                <img
+                    src="../assets/images/nav-img.png"
+                    alt=""
+                    class="nav__img"
+                />
+            </div>
+
+            <button class="nav__toggle" @click="showMenu">
+                <box-icon name="grid-alt"></box-icon>
+            </button>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+
+    data() {
+        return {
+            items: [
+                {
+                    id: 1,
+                    name: 'home',
+                },
+
+                {
+                    id: 2,
+                    name: 'about',
+                },
+                {
+                    id: 3,
+                    name: 'trick',
+                },
+                {
+                    id: 4,
+                    name: 'new',
+                },
+            ],
+        }
+    },
+
+    methods: {
+        showMenu() {
+            this.$refs.menu.style.top = '0'
+        },
+
+        closeMenu() {
+            this.$refs.menu.style.top = '-150%'
+        },
+    },
+}
+</script>
