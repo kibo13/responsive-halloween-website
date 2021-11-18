1f2822cf0cfb7291191e9ddecbe200054aac533f
kibo13 <kimboris1310@gmail.com>
2021-11-19 01:14:56 +0600

    feat: add scroll event that change background of header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <header class="header" id="header">
+        <header class="header" :class="{ 'header--scroll': isActive }">
             <Navbar />
         </header>
 
@@ -16,9 +16,30 @@ import Category from '@/pages/Category.vue'
 
 export default {
     name: 'App',
+
     components: {
         Navbar,
         Category,
+    },
+
+    data() {
+        return {
+            isActive: false,
+        }
+    },
+
+    created() {
+        window.addEventListener('scroll', this.scrollHeader)
+    },
+
+    destroyed() {
+        window.removeEventListener('scroll', this.scrollHeader)
+    },
+
+    methods: {
+        scrollHeader() {
+            this.isActive = window.scrollY >= 50 ? true : false
+        },
     },
 }
 </script>
