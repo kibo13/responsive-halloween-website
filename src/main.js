4af3e41b55e7707d351e1b22761c98b8bb11f7dc
kibo13 <kimboris1310@gmail.com>
2021-11-20 04:17:42 +0600

    feat: add swiper slider

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
+// javascript
 import Vue from 'vue'
 import App from './App.vue'
-
+import { Swiper, Pagination } from 'swiper'
 import 'boxicons'
+
+// styles
 import '@/assets/styles/main.sass'
+
+Swiper.use(Pagination)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: (h) => h(App),
+    render: (h) => h(App),
 }).$mount('#app')
