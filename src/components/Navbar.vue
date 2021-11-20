48eadb480ad2c17186cbdbf000d9500cae06c831
kibo13 <kimboris1310@gmail.com>
2021-11-20 21:44:57 +0600

    refactor: remove extra div tag

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,34 +1,32 @@
 <template>
-    <div>
-        <nav class="nav container">
-            <a href="#" class="nav__logo">
-                <img src="../assets/images/logo.png" alt="" class="nav__logo-img" />
-                Halloween
-            </a>
+    <nav class="nav container">
+        <a href="#" class="nav__logo">
+            <img src="../assets/images/logo.png" alt="" class="nav__logo-img" />
+            Halloween
+        </a>
 
-            <div class="nav__menu" ref="menu">
-                <ul class="nav__list">
-                    <li class="nav__item" v-for="item in items" :key="item.id">
-                        <a class="nav__link" :href="`#${item.name}`" @click="closeMenu">
-                            {{ item.name }}
-                        </a>
-                    </li>
+        <div class="nav__menu" ref="menu">
+            <ul class="nav__list">
+                <li class="nav__item" v-for="item in items" :key="item.id">
+                    <a class="nav__link" :href="`#${item.name}`" @click="closeMenu">
+                        {{ item.name }}
+                    </a>
+                </li>
 
-                    <a href="#" class="button button--ghost">Support</a>
-                </ul>
+                <a href="#" class="button button--ghost">Support</a>
+            </ul>
 
-                <button class="nav__close" @click="closeMenu">
-                    <box-icon name="x" color="white"></box-icon>
-                </button>
-
-                <img src="../assets/images/nav-img.png" alt="" class="nav__img" />
-            </div>
-
-            <button class="nav__toggle" @click="showMenu">
-                <box-icon name="grid-alt" color="white"></box-icon>
+            <button class="nav__close" @click="closeMenu">
+                <box-icon name="x" color="white"></box-icon>
             </button>
-        </nav>
-    </div>
+
+            <img src="../assets/images/nav-img.png" alt="" class="nav__img" />
+        </div>
+
+        <button class="nav__toggle" @click="showMenu">
+            <box-icon name="grid-alt" color="white"></box-icon>
+        </button>
+    </nav>
 </template>
 
 <script>
