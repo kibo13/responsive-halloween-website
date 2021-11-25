ffe85f3e434d99bf1c81fa1a030bb8616f810960
kibo13 <kimboris1310@gmail.com>
2021-11-26 01:54:57 +0600

    feat: add method for active link

--- src/App.vue
+++ src/App.vue
@@ -24,5 +24,34 @@ export default {
         Footer,
         ScrollUp,
     },
+
+    data() {
+        return {
+            sections: null,
+        }
+    },
+
+    methods: {
+        scrollActive() {
+            const scrollY = window.pageYOffset
+
+            this.sections.forEach((current) => {
+                const sectionHeight = current.offsetHeight
+                const sectionTop = current.offsetTop - 58
+                const sectionId = current.getAttribute('id')
+
+                if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                    document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+                } else {
+                    document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+                }
+            })
+        },
+    },
+
+    mounted() {
+        this.sections = document.querySelectorAll('section[id]')
+        window.addEventListener('scroll', this.scrollActive)
+    },
 }
 </script>
